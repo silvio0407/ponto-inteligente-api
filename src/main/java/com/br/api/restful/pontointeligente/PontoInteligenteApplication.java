27fd67a2a158bd643005616d8e314e7a2a09a7ed
silvio040781 <silvio_oliver@hotmail.com>
2018-06-21 05:26:37 -0300

    Adicionado Serviços.

--- src/main/java/com/br/api/restful/pontointeligente/PontoInteligenteApplication.java
+++ src/main/java/com/br/api/restful/pontointeligente/PontoInteligenteApplication.java
@@ -1,4 +1,4 @@
-package com.br.api.restfull.pontointeligente;
+package com.br.api.restful.pontointeligente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/br/api/restful/pontointeligente/enums/PerfilEnum.java
@@ -0,0 +1,7 @@
+package com.br.api.restful.pontointeligente.enums;
+
+public enum PerfilEnum {
+
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
