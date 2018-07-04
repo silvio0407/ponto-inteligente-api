4cb14706826755a3712c3b26d4d25777dd70262b
silvio040781 <silvio_oliver@hotmail.com>
2018-07-03 23:03:58 -0300

    Autenticação e autorização com tokens JWT

---
+++ src/main/java/com/br/api/restful/pontointeligente/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.br.api.restful.pontointeligente.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.br.api.restful.pontointeligente.entities.Funcionario;
+import com.br.api.restful.pontointeligente.security.JwtUserFactory;
+import com.br.api.restful.pontointeligente.services.FuncionarioService;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
